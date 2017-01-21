a07b4dfc19624f5a2d17fe9d98f29a4e9ce283b6
young.kwon <young.kwon@traport.com>
2017-01-21 16:03:22 +0900

    #1 괄호 연산 검증기 : 클래스 세팅

--- test/young/ParenthesesValidatorTest.java
+++ test/young/ParenthesesValidatorTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class ParenthesesTest {
+public class ParenthesesValidatorTest {
 
 	@Before
 	public void setUp() throws Exception {
